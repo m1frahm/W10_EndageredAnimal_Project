688c62a56b2f0a91c63bf25f41b91fe426167d5a
Mifrah Malik <mifrah.malik@gmail.com>
2023-03-22 18:57:01 -0400

    added useEffect into import line to fix error in devtools

--- client/src/components/form.js
+++ client/src/components/form.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Form = (props) => {
   const { initialAnimal = { id: null, nickname: "", r_c_timestamp: "" } } =
